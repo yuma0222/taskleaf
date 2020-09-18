a3895c034700c073a422f9ff580a1e6438c08fe3
yuma0222 <nakamura_poison@icloud.com>
2020-09-18 18:11:23 +0900

    Add validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_111924) do
+ActiveRecord::Schema.define(version: 2020_09_18_032919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

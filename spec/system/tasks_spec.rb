8850b00cd3cd4aac910495ab19be9947101d009c
yuma0222 <nakamura_poison@icloud.com>
2020-09-26 19:58:29 +0900

    add system spec

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧機能表示' do
+  	before do
+  	  user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+  	  FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+  	end
+
+    context 'ユーザーAがログインしているとき' do
+  	  before do
+  	    visit login_path
+  	    fill_in 'メールアドレス', with: 'a@example.com'
+  	    fill_in 'パスワード', with: 'password'
+  	    click_button 'ログインする'
+  	  end
+
+  	  it 'ユーザーAが作成したタスクが表示される' do
+  	    expect(page).to have_content '最初のタスク'
+  	  end
+  	end
+  end
+end

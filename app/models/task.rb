dbd51142d67c759e8e456a3117c6df1d008ae5e5
yuma0222 <nakamura_poison@icloud.com>
2020-09-30 14:05:46 +0900

    add new system spec

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-	validates :name, presence: true
+	#validates :name, presence: true
 	validates :name, length: { maximum: 30 }
 	validate :validate_name_not_including_comma
 

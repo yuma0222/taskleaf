366b2e9e57f0a4339ab36b508a7b8d18c4605791
yuma0222 <nakamura_poison@icloud.com>
2020-10-06 16:31:23 +0900

    add import export

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,6 +10,27 @@ class Task < ApplicationRecord
       []
     end
 
+    def self.csv_attributes
+    	["name", "description", "description", "created_at", "updated_at" ]
+    end
+
+    def self.generate_csv
+      CSV.generate(headers: true) do |csv|
+    	csv << csv_attributes
+    	all.each do |task|
+    	  csv << csv_attributes.map{|attr| task.send(attr) }
+    	end
+      end
+    end
+
+    def self.import(file)
+      CSV.foreach(file.path, headers: true) do |row|
+      	task = new
+      	task.attributes = row.to_hash.slice(*csv_attributes)
+      	task.save!
+      end
+    end
+
 	validates :name, length: { maximum: 30 }
 	validate :validate_name_not_including_comma
 

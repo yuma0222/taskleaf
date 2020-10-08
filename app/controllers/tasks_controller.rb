ced678f4f4b5a31dec63eff98ee1ca8228e23b6a
yuma0222 <nakamura_poison@icloud.com>
2020-10-08 11:28:56 +0900

    add sidekiq

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,7 @@ class TasksController < ApplicationController
 
   	if @task.save
       TaskMailer.creation_email(@task).deliver_now
+      SampleJob.perform_later
   	  redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
